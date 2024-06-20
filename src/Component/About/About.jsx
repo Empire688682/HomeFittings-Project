fff7c1da085ce5a19c075a02c83670cd966c028e
Empire688682 <asehindej@gmail.com>
2024-06-21 00:56:05 +0100

    new update

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import { useInView } from "react-intersection-observer";
 import './About.css';
 import about_Img from '../Assets/About_Img.png'
 
 const About = () => {
+  const {ref:cl1Ref, inView:cl1View} = useInView({triggerOnce:true});
+  const {ref:cl2Ref, inView:cl2View} = useInView({triggerOnce:true});
+  const {ref:cl3Ref, inView:cl3View} = useInView({triggerOnce:true});
+  const {ref:cl4Ref, inView:cl4View} = useInView({triggerOnce:true});
   return (
     <div className='about'>
       <div className="left_colum">
-        <img src={about_Img} alt="IMG" />
+        <img ref={cl1Ref} className={cl1View? "img observer":"img"} src={about_Img} alt="IMG" />
       </div>
       <div className="right_colum">
-        <h1>We offer the best quality fittings</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat neque atque, qui et mollitia soluta ratione officia delectus tempora, incidunt facilis quibusdam reiciendis illum accusantium, molestiae eius. Cupiditate, libero amet!</p>
-        <div className="experience_all">
+        <h1 ref={cl2Ref} className={cl2View? "img observer":"img"} >We offer the best quality fittings</h1>
+        <p ref={cl3Ref} className={cl3View? "img observer":"img"}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat neque atque, qui et mollitia soluta ratione officia delectus tempora, incidunt facilis quibusdam reiciendis illum accusantium, molestiae eius. Cupiditate, libero amet!</p>
+        <div ref={cl4Ref} className={cl4View? "experience_all observer":"experience_all"}>
           <div className="three_col">
-            <h4>Happy client</h4>
+            <h4 >Happy client</h4>
             <h2>34+</h2>
           </div>
           <div className="three_col">
-            <h4>Happy client</h4>
+            <h4 >Happy client</h4>
             <h2>20y</h2>
           </div>
           <div className="three_col">
