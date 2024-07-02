06254115b73bf33c52e7972de2dbad00030c1008
Empire688682 <asehindej@gmail.com>
2024-07-03 00:19:07 +0100

    new file

---
+++ src/Component/infinteImageData.js
@@ -0,0 +1,48 @@
+import image_1 from './Assets/door_1.png'
+import image_2 from './Assets/door_2.png'
+import image_3 from './Assets/door_4.png'
+import image_4 from './Assets/door_4.png'
+import image_5 from './Assets/door_5.png'
+import image_6 from './Assets/sanitry_1.png'
+import image_7 from './Assets/sanitry_2.png'
+import image_8 from './Assets/sanitry_3.png'
+import image_9 from './Assets/sanitry_4.png'
+import image_10 from './Assets/sanitry_5.png'
+import image_11 from './Assets/tile_1.png'
+import image_12 from './Assets/tile_2.png'
+import image_13 from './Assets/tile_3.png'
+import image_14 from './Assets/tile_4.png'
+import image_15 from './Assets/tile_5.png'
+import about_image from './Assets/About_Img.png'
+import image_16 from './Assets/sanitry_7.png'
+import image_18 from './Assets/sanitry_8.png'
+import image_19 from './Assets/sanitry_9.png'
+import image_20 from './Assets/sanitry_10.png'
+import image_17 from './Assets/sanitry_6.png'
+
+
+const all_images = [
+    about_image,
+    image_1,
+    image_2,
+    image_3,
+    image_4,
+    image_5,
+    image_6,
+    image_7,
+    image_8,
+    image_9,
+    image_16,
+    image_10,
+    image_11,
+    image_12,
+    image_13,
+    image_14,
+    image_15,
+    image_17,
+    image_18,
+    image_19,
+    image_20
+  ];
+  
+  export default all_images;
